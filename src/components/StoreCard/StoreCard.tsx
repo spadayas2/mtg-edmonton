47a33ae52a4f724585ae0c63a09411009dd37e9e
spadayas2 <spadayas2@gmail.com>
2024-12-09 16:22:39 -0700

    update: fix

--- src/components/StoreCard/StoreCard.tsx
+++ src/components/StoreCard/StoreCard.tsx
@@ -1,93 +1,52 @@
-import classes from "./StoreCard.module.css";
+import styles from "./StoreCard.module.css";
 import { FaPhoneAlt } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
-import { PiCompassRoseFill } from "react-icons/pi";
 import { Store } from "../Types/Types";
 
-export default function StoreCard({
-store
-}: {
-store:Store;
-}) {
+export default function StoreCard({ store, onDetailsClick }: { store: Store, onDetailsClick: (storeData: Store) => void }) {
   return (
-    <div
-      style={{
-        border: "1px solid black",
-        boxShadow: "2px 1.5px black",
-        width:"100%",
-        overflow: "hidden",
-        backgroundColor: "white",
-      }}
-    >
-      <div
-        style={{
-          padding: "1.5rem",
-        }}
-      >
-        <div style={{display:"flex"}}>
-          <div
-            style={{
-              fontFamily: "'Protest Strike', sans-serif",
-              fontSize: "3rem",
-            }}
-          >
-            {store.storeName}
-          </div>
-          
+    <div className={styles.card}>
+      <div className={styles.padding}>
+        <div className={styles.flex}>
+          <div className={styles.storeName}>{store.storeName}</div>
         </div>
 
-        <div
-          style={{
-            fontFamily: "'Rubik', sans-serif",
-            fontSize: "1.2rem",
-            marginTop: "1rem",
-          }}
-        >
-          <div style={{ display: "flex", gap: "1rem" }}>
+        <div className={styles.storeDetails}>
+          <div className={styles.flexGap}>
             <div>
               <FaPhoneAlt color="#003c62" />
             </div>
             <div>{store.phoneNumber}</div>
           </div>
-          <div style={{ display: "flex", gap: "1rem" }}>
+          <div className={styles.flexGap}>
             <div>
               <FaLocationDot color="#003c62" />
             </div>
             <div>{store.address}</div>
           </div>
-          <div style={{ display: "flex", gap: "1rem" }}>
-            <div>
-              <PiCompassRoseFill color="#003c62" />
-            </div>
-            <div>{store.zone}</div>
-          </div>
-          
         </div>
       </div>
       <div>
-        <div
-          style={{
-            display: "flex",
-          }}
-        >
+        <div className={styles.flex}>
           {store.websiteURL && (
-            <a href={store.websiteURL} target="_blank" style={{ width: "50%" }}>
-              <button className={classes.button + " " + classes.website}>
+            <a
+              href={store.websiteURL}
+              target="_blank"
+              className={styles.width50}
+            >
+              <button className={styles.button + " " + styles.website}>
                 Website
               </button>
             </a>
           )}
-          {store.googleDirectionsURL && (
-            <a
-              href={store.googleDirectionsURL}
-              target="_blank"
-              style={{ width: "50%" }}
+                      <a
+              className={styles.width50}
             >
-              <button className={classes.button + " " + classes.directions}>
-                Directions
+              <button className={styles.button + " " + styles.directions}
+              onClick={()=>onDetailsClick(store)}>
+                Details
               </button>
             </a>
-          )}
         </div>
       </div>
     </div>
